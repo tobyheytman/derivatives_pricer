2242091ae54e9c03f0d2e6a806c658ba44049e61
TobyH127 <t.heytman@student.uq.edu.au>
2026-02-17 21:42:27 +1000

    MarketData Class

--- init.py
+++ init.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from enum import Enum
 
 print("This is an initial test")
+
+
+@dataclass
+class MarketData(Enum):
+    price: float
+    volume: float
+    timestamp: pd.Timestamp
