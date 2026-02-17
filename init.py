c302d61ed9cf729c88ef818955fa2f160113a6f8
TobyH127 <t.heytman@student.uq.edu.au>
2026-02-17 21:59:54 +1000

    Initial Changes

--- init.py
+++ init.py
@@ -1,3 +1,18 @@
+from enum import Enum, auto
+from abc import ABC, abstractmethod
+from typing import Dict, Final
 import numpy as np
-from typing import Final, Dict
 from scipy.interpolate import interp1d
+from .instruments import ExerciseStyle, OptionType, YearFraction
+
+
+class DayCountConvertion(Enum):
+    ACT_360 = auto()
+    ACT_365 = auto()
+    THIRTY_360 = auto()
+
+
+class YieldCurve(ABC):
+    @abstractmethod
+    def discount_factor(self, t: YearFraction) -> float:
+        pass
--- instruments.py
+++ instruments.py
@@ -2,6 +2,7 @@ import numpy as np
 from enum import Enum, auto
 from typing import NewType, Protocol, runtime_checkable, Final
 
+YearFraction = NewType("YearFraction", float)
 
 class OptionType(Enum):
     CALL = auto()
